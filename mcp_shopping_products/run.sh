8b8af1d8d4540951fbf975967c077b7dab12a319
eragon02424 <52879412+eragon02424@users.noreply.github.com>
2026-07-05 10:53:08 +0200

    feat: scaffold mcp_shopping_products addon (config, Dockerfile, build.yaml, run.sh, requirements)

---
+++ mcp_shopping_products/run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/with-contenv bashio
+
+export GROCY_HOST=$(bashio::config 'grocy_host')
+export GROCY_LOCATION_ID=$(bashio::config 'location_id')
+export GROCY_QU_PURCHASE=$(bashio::config 'qu_id_purchase')
+export GROCY_QU_STOCK=$(bashio::config 'qu_id_stock')
+
+bashio::log.info "Starting MCP Shopping Products on port 8769, Grocy host: ${GROCY_HOST}"
+
+exec python3 /server.py
